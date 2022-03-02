dffb1a23c4a2e8a857618da7ca445b771d34ca70
BEK <76460501+beksobirov@users.noreply.github.com>
2022-03-02 21:57:59 +0500

    Update stem_and_lemma.py

--- stem_and_lemma.py
+++ stem_and_lemma.py
@@ -1,6 +1,14 @@
-import nltk
-wn = nltk.WordNetLemmatizer()
-ps = nltk.PorterStemmer()
+# 1st method import nltk
+# import nltk
+# wn = nltk.WordNetLemmatizer()
+# ps = nltk.PorterStemmer()
+# End 1st method
+
+# 2nd method import nltk
+from nltk import WordNetLemmatizer, PorterStemmer
+wn = WordNetLemmatizer()
+ps = PorterStemmer()
+# End 2nd method
 
 #dir(wn)
 #dir(ps)
